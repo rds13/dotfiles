c9dc2024c4ddacc15b72855961fb6769e7d37b57
Romuald du Song <romuald.dusong@deveryware.com>
2013-11-10 17:45:58 +0100

    document alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,8 @@
 alias reload!='. ~/.zshrc'
 
-alias psup='_() { ps acxo etime,command | grep -i -- "$1"; }; _'
+if [ "$(uname -s)" = "Darwin" ]
+then
+  # http://hints.macworld.com/comment.php?mode=view&cid=131169
+  # See how long a given process has been running
+  alias psup='_() { ps acxo etime,command | grep -i -- "$1"; }; _'
+fi
