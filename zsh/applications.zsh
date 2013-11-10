60122933889dff2e38bfca39a98f27f71a9116b0
Romuald du Song <romuald.dusong@deveryware.com>
2013-11-10 18:06:05 +0100

    syntax

--- zsh/applications.zsh
+++ zsh/applications.zsh
@@ -1,5 +1,6 @@
 # from http://www.iterm2.com/#/section/documentation/shell_tips
-if [ "$(uname -s)" == "Darwin" ]; then
+if [[ "$(uname -s)" == "Darwin" ]]
+then
   compctl -f -x 'p[2]' -s "`/bin/ls -d1 /Applications/*/*.app /Applications/*.app | sed 's|^.*/\([^/]*\)\.app.*|\\1|;s/ /\\\\ /g'`" -- open
   alias run='open -a'
 fi
