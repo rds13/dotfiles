7546feb6452efb0aa647fc7c047e4caff4e2cf1d
rds13 <rds@Panther.local>
2014-01-07 09:22:21 +0100

    check for osx before installing

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -5,14 +5,17 @@
 # This installs some of the common dependencies needed (or at least desired)
 # using Homebrew.
 
-# Check for Homebrew
-if test ! $(which brew)
+if [ "$(uname -s)" == "Darwin" ]
 then
-  echo "  Installing Homebrew for you."
-  ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)" > /tmp/homebrew-install.log
-fi
+  # Check for Homebrew
+  if test ! $(which brew)
+  then
+    echo "  Installing Homebrew for you."
+    ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)" > /tmp/homebrew-install.log
+  fi
 
-# Install homebrew packages
-brew install grc coreutils spark
+  # Install homebrew packages
+  brew install grc coreutils spark gawk
+fi
 
 exit 0
