b4b17476951baca4dcf529eb1d4a5198f3fdeb38
rds13 <rds@Panther.local>
2014-01-07 10:48:05 +0100

    reorganize content in specific files

--- system/env.zsh
+++ system/env.zsh
@@ -3,3 +3,16 @@ if [[ "$EDITOR" == "" ]] ; then
   # Use sublime for my editor.
   export EDITOR='subl'
 fi
+# Mac OS
+if [ "$(uname -s)" = "Darwin" ]
+then
+  export XML_CATALOG_FILES=$(brew --prefix)/etc/xml/catalog
+fi
+# Gnu GPG vim
+export GPG_TTY=`tty`
+
+export JAVA_MAIL_SERVER="smtp.free.fr"
+export PID_DIR=${HOME}/deveryflow
+export LOG_DIR=${HOME}/deveryflow
+export M2_HOME=/usr/share/maven
+#
--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,14 @@
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$DOTS/bin:$PATH"
+export PATH="./bin:$HOME/bin:/usr/local/bin:/usr/local/sbin:$DOTS/bin:$PATH"
+export MANPATH="/usr/local/man:/usr/local/git/man:$MANPATH"
+if [ -d /usr/local/mysql ] ; then
+  export PATH=/usr/local/mysql/bin:$PATH
+  export MANPATH="/usr/local/mysql/man:$MANPATH"
+fi
 
-export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+#
+# Mac OS
+if [ "$(uname -s)" = "Darwin" ]
+then
+  # Use JDK 1.6
+  export PATH=/System/Library/Frameworks/JavaVM.framework/Versions/1.6.0/Commands:$PATH
+fi
