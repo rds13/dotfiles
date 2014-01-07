582711321508c14d57b430dddd529df128c1193d
rds13 <rds@panther.tique.grp>
2014-01-07 21:35:42 +0100

    use double-quotes

--- system/path.zsh
+++ system/path.zsh
@@ -1,7 +1,7 @@
-export PATH="./bin:$HOME/bin:/usr/local/bin:/usr/local/sbin:$DOTS/bin:$PATH"
+export PATH="$HOME/bin:/usr/local/bin:/usr/local/sbin:$DOTS/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/git/man:$MANPATH"
 if [ -d /usr/local/mysql ] ; then
-  export PATH=/usr/local/mysql/bin:$PATH
+  export PATH="/usr/local/mysql/bin:$PATH"
   export MANPATH="/usr/local/mysql/man:$MANPATH"
 fi
 
@@ -10,5 +10,5 @@ fi
 if [ "$(uname -s)" = "Darwin" ]
 then
   # Use JDK 1.6
-  export PATH=/System/Library/Frameworks/JavaVM.framework/Versions/1.6.0/Commands:$PATH
+  export PATH="/System/Library/Frameworks/JavaVM.framework/Versions/1.6.0/Commands:$PATH"
 fi
