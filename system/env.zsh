62a034c03873353caa0c8987d042dcdb2cf6e606
Romuald du Song <rds@panther.tique.grp>
2014-01-17 15:44:34 +0100

    set JAVA_HOME and remove useless M2_HOME

--- system/env.zsh
+++ system/env.zsh
@@ -3,6 +3,7 @@ if [[ "$EDITOR" == "" ]] ; then
   # Use sublime for my editor.
   export EDITOR='subl'
 fi
+
 # Mac OS
 if [ "$(uname -s)" = "Darwin" ]
 then
@@ -10,15 +11,17 @@ then
   if [ -d /opt/boxen/ ]
   then
     export XML_CATALOG_FILES=/opt/boxen/homebrew/etc/xml/catalog
+    #export M2_HOME=`brew --prefix maven30`/libexec
   else
     export XML_CATALOG_FILES=/etc/xml/catalog
   fi
+  export JAVA_HOME=`/usr/libexec/java_home`
 fi
+
 # Gnu GPG vim
 export GPG_TTY=`tty`
 
 export JAVA_MAIL_SERVER="smtp.free.fr"
 export PID_DIR=${HOME}/deveryflow
 export LOG_DIR=${HOME}/deveryflow
-export M2_HOME=/usr/share/maven
 #
