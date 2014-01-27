c6373f09b457e2cb0ce627af74dbc9df36cbc498
Romuald du Song <romuald.dusong@deveryware.com>
2014-01-27 10:37:41 +0100

    add configuration for GraphViz

--- system/env.zsh
+++ system/env.zsh
@@ -11,6 +11,7 @@ then
   if [ -d /opt/boxen/ ]
   then
     export XML_CATALOG_FILES=/opt/boxen/homebrew/etc/xml/catalog
+    export GRAPHVIZ_DOT=/opt/boxen/homebrew/bin/dot
     #export M2_HOME=`brew --prefix maven30`/libexec
   else
     export XML_CATALOG_FILES=/etc/xml/catalog
