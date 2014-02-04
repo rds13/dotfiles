44e882ffcff6303c74618ae039742b9bf4f07874
Romuald du Song <romuald.dusong@deveryware.com>
2014-02-04 21:07:37 +0100

    use script shell syntax

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -5,10 +5,10 @@
 # This installs some of the common dependencies needed (or at least desired)
 # using Homebrew.
 
-if [ "$(uname -s)" == "Darwin" ]
+if [ "$(uname -s)" = "Darwin" ]
 then
   # Check for Homebrew
-  if test ! $(which brew)
+  if [ ! $(which brew) ]
   then
     echo "  Installing Homebrew for you."
     ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)" > /tmp/homebrew-install.log
