a0b0d1d1811998ca2a2af82b93cb2810c0d393d7
Romuald du Song <romuald.dusong@deveryware.com>
2014-06-13 09:13:57 +0200

    skip homebrew install if we install boxen

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -5,13 +5,18 @@
 # This installs some of the common dependencies needed (or at least desired)
 # using Homebrew.
 
+cd "$(dirname $)"
 if [ "$(uname -s)" = "Darwin" ]
 then
-  # Check for Homebrew
-  if [ ! $(which brew) ]
+  # Don't try to install homebrew if boxen is doing the same
+  if ! [ -f "../boxen/install.sh" ]
   then
-    echo "  Installing Homebrew for you."
-    ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)" > /tmp/homebrew-install.log
+    # Check for Homebrew
+    if [ ! $(which brew >/dev/null 2>&1) ]
+    then
+      echo "  Installing Homebrew for you."
+      ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)" > /tmp/homebrew-install.log
+    fi
   fi
 
   # Install homebrew packages
