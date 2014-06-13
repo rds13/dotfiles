531ac5e1790a671f20a4f906aa7fbce3854c527b
Romuald du Song <romuald.dusong@deveryware.com>
2014-06-13 18:47:00 +0200

    give specific information about installed components

--- ruby/install.sh
+++ ruby/install.sh
@@ -9,12 +9,12 @@ fi
 
 if test ! $(which rbenv)
 then
-  echo "  Installing rbenv for you."
+  echo "  Installing rbenv for you via homebrew."
   brew install rbenv > /tmp/rbenv-install.log
 fi
 
 if test ! $(which ruby-build)
 then
-  echo "  Installing ruby-build for you."
+  echo "  Installing ruby-build for you via homebrew."
   brew install ruby-build > /tmp/ruby-build-install.log
 fi
