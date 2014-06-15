feb204799cc4e0dc0dba68af208e45a9931c6c6a
Romuald du Song <romuald.dusong@deveryware.com>
2014-06-15 15:14:55 +0200

    correct link for vundles.vim

--- vim/install.sh
+++ vim/install.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 #
 # Install vim dependencies : vundle
+BASE="$(pwd)"
 [ -d ~/.vim ] || mkdir ~/.vim
 [ -d ~/.vim/bundle ] || mkdir ~/.vim/bundle
 [ -d ~/.vim/bundle/vundle ] || git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 [ -d ~/.vim/after ] || mkdir ~/.vim/after
 [ -d ~/.vim/after/plugin ] || mkdir ~/.vim/after/plugin
 cp vim/tabular.vim ~/.vim/after/plugin
-ln -s vim/vundles.vim ~/.vim/vundles.vim
+ln -s "${BASE}/vim/vundles.vim" ~/.vim/vundles.vim
 vim -u ~/.vim/vundles.vim --noplugin -N "+set hidden" "+syntax on" +BundleClean +BundleInstall +qall
