26464b1eb69356728df26cc9181d4aa1532f4ca8
Romuald du Song <romuald.dusong@deveryware.com>
2014-06-15 14:24:52 +0200

    link oh-my-zsh install to ~/.oh-my-zsh

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -4,12 +4,11 @@
 #
 
 cd "$(dirname $)"
-set -e
 
 if [ ! -d ~/.oh-my-zsh ]
 then
   # install oh-my-zsh in same dir that dotfiles
   cd ..
   git clone https://github.com/robbyrussell/oh-my-zsh.git oh-my-zsh
-  ln -s ~/.oh-my-zsh oh-my-zsh
+  ln -s oh-my-zsh ${HOME}/.oh-my-zsh
 fi
