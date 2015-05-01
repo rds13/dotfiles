9fa6db4a41a9c13af1d89eb3baf7de2e0b35a8f0
Romuald du Song <romuald.dusong@deveryware.com>
2015-05-01 18:54:26 +0200

    add installation instructions for linux

--- ruby/install.sh
+++ ruby/install.sh
@@ -2,23 +2,32 @@
 # Install script for ruby via homebrew
 
 # OSX-only stuff. Abort if not OSX.
-[[ "$OSTYPE" =~ ^darwin ]] || exit 0
-
-# Check for Homebrew
-if test ! $(which brew)
+if [[ "$OSTYPE" =~ ^darwin ]]
 then
-  echo "  You should have homebrew installed before installing ruby."
-  exit
-fi
 
-if test ! $(which rbenv)
-then
-  echo "  Installing rbenv for you via homebrew."
-  brew install rbenv > /tmp/rbenv-install.log
-fi
+  # Check for Homebrew
+  if test ! $(which brew)
+  then
+    echo "  You should have homebrew installed before installing ruby."
+    exit
+  fi
 
-if test ! $(which ruby-build)
-then
-  echo "  Installing ruby-build for you via homebrew."
-  brew install ruby-build > /tmp/ruby-build-install.log
+  if test ! $(which rbenv)
+  then
+    echo "  Installing rbenv for you via homebrew."
+    brew install rbenv > /tmp/rbenv-install.log
+  fi
+
+  if test ! $(which ruby-build)
+  then
+    echo "  Installing ruby-build for you via homebrew."
+    brew install ruby-build > /tmp/ruby-build-install.log
+  fi
+
+else
+  git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+  git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+  export PATH="$HOME/.rbenv/bin:$PATH"
+  eval "$(rbenv init -)"
+  rbenv rehash 2>/dev/null
 fi
