08752fc0b9ae4824a00916413e125841e34d1fa3
Romuald du Song <romuald.dusong@deveryware.com>
2016-04-24 17:44:39 +0200

    don't reinstall if everything seems ok

--- boxen/install.sh
+++ boxen/install.sh
@@ -2,19 +2,18 @@
 
 # OSX-only stuff. Abort if not OSX.
 [[ "$OSTYPE" =~ ^darwin ]] || exit 0
+[[ -n "$BOXEN_HOME" ]] || exit 0
 
-if ! [ -d /opt/boxen ]
+BOXEN_HOME=${BOXEN_HOME:-/opt/boxen}
+echo "I need sudo for boxen installation at /opt/boxen"
+[[ -d ""$BOXEN_HOME" ]] || sudo mkdir -p ""$BOXEN_HOME"
+sudo chown ${USER}:staff "$BOXEN_HOME"
+echo "I will need your github credential to access our-boxen repo from Deveryware"
+git clone git@github.com:Deveryware/our-boxen.git "$BOXEN_HOME/repo"
+if [ -d "$BOXEN_HOME/repo" ]
 then
-  echo "I need sudo for /opt/boxen installation"
-  sudo mkdir -p /opt/boxen
-  sudo chown ${USER}:staff /opt/boxen
-  echo "I will need your github credential to access our-boxen repo from Deveryware"
-  git clone git@github.com:Deveryware/our-boxen.git /opt/boxen/repo
-  if [ -d /opt/boxen/repo ]
-  then
-    cd /opt/boxen/repo
-    ./script/boxen --no-fde
-  else
-    echo "Sorry something went wrong, please retry"
-  fi
+  cd "$BOXEN_HOME/repo"
+  ./script/boxen --no-fde
+else
+  echo "Sorry something went wrong, please retry"
 fi
