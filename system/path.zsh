220bc4a007d5eba60cc5f52c869aa75ec362e844
Romuald du Song <romuald.dusong@deveryware.com>
2016-10-24 15:22:20 +0200

    fix path order

--- system/path.zsh
+++ system/path.zsh
@@ -1,5 +1,5 @@
 #
-export PATH="$HOME/bin:/usr/local/bin:/usr/local/sbin:$DOTS/bin:$PATH"
+export PATH="$HOME/bin:$DOTS/bin:/usr/local/bin:/usr/local/sbin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/git/man:$MANPATH"
 if [ -d /usr/local/mysql ] ; then
   export PATH="/usr/local/mysql/bin:$PATH"
