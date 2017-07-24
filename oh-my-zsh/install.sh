33d5d21c8c1080d532851248724580d962914bc5
Romuald du Song <rdusong@tique.org>
2017-07-24 22:30:40 +0200

    optimize git clone

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -10,6 +10,6 @@ if [ ! -d "${HOME}/.oh-my-zsh" ]
 then
   # install oh-my-zsh in same dir that dotfiles
   cd ..
-  git clone https://github.com/robbyrussell/oh-my-zsh.git oh-my-zsh
+  git clone  --depth 1 https://github.com/robbyrussell/oh-my-zsh.git oh-my-zsh
   ln -s "${BASE}/../oh-my-zsh" "${HOME}/.oh-my-zsh"
 fi
