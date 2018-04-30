4418e2f670426ddf612c0854eab709a85c1511c7
Romuald du Song <romuald.dusong@deveryware.com>
2018-04-30 10:56:46 +0200

    don't install homebrew, remove obsolete package

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -4,6 +4,9 @@
 #
 # This installs some of the common dependencies needed (or at least desired)
 # using Homebrew.
+#
+# Homebrew installation is out of scope here
+#
 
 # OSX-only stuff. Abort if not OSX.
 [[ "$OSTYPE" =~ ^darwin ]] || exit 0
@@ -11,17 +14,10 @@
 cd "$(dirname $)"
 
 # Check for Homebrew
-if [ ! $(which brew >/dev/null 2>&1) ]
-then
-  echo "  Installing Homebrew for you."
-  ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)" > /tmp/homebrew-install.log
-fi
-
-# Check for Homebrew
 if command -v brew >/dev/null;
 then
   # Install homebrew packages
-  brew install grc coreutils spark gawk maven30 gnupg2 asciidoc graphviz
+  brew install grc coreutils spark gawk gnupg2 asciidoc graphviz
 fi
 
 exit 0
