c25ec843c68f60a5b5c72e2b7df93efdad44fe5f
rds13 <rdusong@tique.org>
2018-04-30 16:05:26 +0200

    fix powerlevel9k check test

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -13,8 +13,9 @@ then
   git clone  --depth 1 https://github.com/robbyrussell/oh-my-zsh.git oh-my-zsh
   ln -s "${BASE}/../oh-my-zsh" "${HOME}/.oh-my-zsh"
 fi
+
+cd "$(dirname $)"/..
 if [ ! -d powerlevel9k ]
 then
-  cd "$(dirname $)"/..
   git clone --depth 1 https://github.com/bhilburn/powerlevel9k.git powerlevel9k
 fi
