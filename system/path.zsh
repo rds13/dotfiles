e583e93760a3c60b0fce93450d7d33edcc027a57
Romuald du Song <romuald.dusong@deveryware.com>
2018-09-17 20:48:10 +0200

    handle brew install

--- system/path.zsh
+++ system/path.zsh
@@ -5,14 +5,17 @@ if [ -d /usr/local/mysql ] ; then
   export PATH="/usr/local/mysql/bin:$PATH"
   export MANPATH="/usr/local/mysql/man:$MANPATH"
 fi
-HB=$(brew --prefix)
-if [ -d "${HB}/opt/gnupg@2.0/bin" ] ; then
-  export PATH="${HB}/opt/gnupg@2.0/bin:$PATH"
-fi
-if [ -d "${HB}/opt/gpg-agent/bin" ] ; then
-  export PATH="${HB}/opt/gpg-agent/bin:$PATH"
-fi
-# See https://docs.brew.sh/Homebrew-and-Python.html
-if [ -d "${HB}/opt/python/libexec/bin" ] ; then
-  export PATH="${HB}/opt/python/libexec/bin:$PATH"
+
+if command -v brew >/dev/null ; then
+  HB=$(brew --prefix)
+  if [ -d "${HB}/opt/gnupg@2.0/bin" ] ; then
+    export PATH="${HB}/opt/gnupg@2.0/bin:$PATH"
+  fi
+  if [ -d "${HB}/opt/gpg-agent/bin" ] ; then
+    export PATH="${HB}/opt/gpg-agent/bin:$PATH"
+  fi
+  # See https://docs.brew.sh/Homebrew-and-Python.html
+  if [ -d "${HB}/opt/python/libexec/bin" ] ; then
+    export PATH="${HB}/opt/python/libexec/bin:$PATH"
+  fi
 fi
