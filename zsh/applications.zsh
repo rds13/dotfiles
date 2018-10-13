880a88665ef4ad3278ff00a29caf9f0e8fcd9d6c
Romuald du Song <romuald.dusong@deveryware.com>
2018-10-13 13:58:56 +0200

    add zsh syntax highlight

--- zsh/applications.zsh
+++ zsh/applications.zsh
@@ -4,3 +4,8 @@ then
   compctl -f -x 'p[2]' -s "`/bin/ls -d1 /Applications/*/*.app /Applications/*.app | sed 's|^.*/\([^/]*\)\.app.*|\\1|;s/ /\\\\ /g'`" -- open
   alias run='open -a'
 fi
+
+if [[ -f /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]]
+then
+  source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+fi
