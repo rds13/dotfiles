daa7e1b9f37bbc667d4384411138fc58020d42e7
Romuald du Song <rdusong@tique.org>
2018-10-15 09:03:59 +0200

    fix powerlevel9k install path

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -6,15 +6,14 @@
 cd "$(dirname $)"
 BASE="$(pwd)"
 
+cd ..
 if [ ! -d "${HOME}/.oh-my-zsh" ]
 then
   # install oh-my-zsh in same dir that dotfiles
-  cd ..
   git clone  --depth 1 https://github.com/robbyrussell/oh-my-zsh.git oh-my-zsh
   ln -s "${BASE}/../oh-my-zsh" "${HOME}/.oh-my-zsh"
 fi
 if [ ! -d powerlevel9k ]
 then
-  cd "$(dirname $)"/..
   git clone --depth 1 https://github.com/bhilburn/powerlevel9k.git powerlevel9k
 fi
